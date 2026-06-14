6a7e51fde9a586f8380b76b21a16ebcbb5456690
YatsenNo1 <he_zhongshan@163.com>
2026-06-14 17:44:10 +0800

    feat: 智能体配置表设计

--- ai-agent-scaffold-domain/src/main/java/com/yat_sen/ai/domain/agent/adapter/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 外部接口适配器层；当需要调用外部接口时，则创建出这一层，并定义接口，之后由基础设施层的 adapter 层具体实现
- */
-<<<<<<<< HEAD:ai-agent-scaffold-domain/src/main/java/com/yat_sen/ai/domain/xxx/adapter/package-info.java
-package com.yat_sen.ai.domain.xxx.adapter;
-========
-package com.yat_sen.ai.domain.agent.adapter;
->>>>>>>> ef1e617 (feat: 智能体配置表设计):ai-agent-scaffold-domain/src/main/java/com/yat_sen/ai/domain/agent/adapter/package-info.java
